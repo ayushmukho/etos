6cecd73bed78099d4d8e24e0fc8998e3454a73fa
Ayush Mukhopadhyay <52779201+ayushmukho@users.noreply.github.com>
2021-12-26 11:03:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbbHdRn21Oh3cKuUNTI7UPlUFOg0gLZgo",
-  authDomain: "rn-uber-2f806.firebaseapp.com",
-  projectId: "rn-uber-2f806",
-  storageBucket: "rn-uber-2f806.appspot.com",
-  messagingSenderId: "38901450081",
-  appId: "1:38901450081:web:4b741d1a7731bb22993120",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
