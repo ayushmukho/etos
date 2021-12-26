22c5e1cd999416cdd5c59456720062489fe34dcf
ayushmukho <ayushmukho@gmail.com>
2021-12-26 13:31:57 +0530

    Worked on Firebase Integration

--- components/restaurantDetail/MenuItems.js
+++ components/restaurantDetail/MenuItems.js
@@ -4,46 +4,6 @@ import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { Divider } from "react-native-elements";
 import { useDispatch, useSelector } from "react-redux";
 
-const foods = [
-  {
-    title: "Lasagna",
-    description: "With butter lettuce, tomato and sauce bechamel",
-    price: "$13.50",
-    image:
-      "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
-  },
-  {
-    title: "Tandoori Chicken",
-    description:
-      "Amazing Indian dish with tenderloin chicken off the sizzles 🔥",
-    price: "$19.20",
-    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
-  },
-  {
-    title: "Chilaquiles",
-    description:
-      "Chilaquiles with cheese and sauce. A delicious mexican dish 🇲🇽",
-    price: "$14.50",
-    image:
-      "https://i2.wp.com/chilipeppermadness.com/wp-content/uploads/2020/11/Chilaquales-Recipe-Chilaquiles-Rojos-1.jpg",
-  },
-  {
-    title: "Chicken Caesar Salad",
-    description:
-      "One can never go wrong with a chicken caesar salad. Healthy option with greens and proteins!",
-    price: "$21.50",
-    image:
-      "https://images.themodernproper.com/billowy-turkey/production/posts/2019/Easy-italian-salad-recipe-10.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1614096227&s=c0f63a30cef3334d97f9ecad14be51da",
-  },
-  {
-    title: "Lasagna",
-    description: "With butter lettuce, tomato and sauce bechamel",
-    price: "$13.50",
-    image:
-      "https://thestayathomechef.com/wp-content/uploads/2017/08/Most-Amazing-Lasagna-2-e1574792735811.jpg",
-  },
-];
-
 const styles = StyleSheet.create({
   menuItemStyle: {
     flexDirection: "row",
@@ -56,7 +16,12 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function MenuItems({ restaurantName }) {
+export default function MenuItems({
+  restaurantName,
+  foods,
+  hideCheckbox,
+  marginLeft,
+}) {
   const dispatch = useDispatch();
   const selectItem = (items, checkboxValue) => {
     dispatch({
@@ -69,25 +34,30 @@ export default function MenuItems({ restaurantName }) {
     });
   };
 
-  const cartItems = useSelector((state) => state.cartReducer.selectedItems.items)
+  const cartItems = useSelector(
+    (state) => state.cartReducer.selectedItems.items
+  );
 
-  const isFoodInCart = (food, cartItems) => (
-    Boolean(cartItems.find((item) => item.title === food.title))
-  )
+  const isFoodInCart = (food, cartItems) =>
+    Boolean(cartItems.find((item) => item.title === food.title));
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       {foods.map((food, index) => (
         <View key={index}>
           <View style={styles.menuItemStyle}>
-            <BouncyCheckbox
-              iconStyle={{ borderColor: "lightgray", borderRadius: 0 }}
-              fillColor="green"
-              onPress={(checkboxValue) => selectItem(food, checkboxValue)}
-              isChecked={isFoodInCart(food, cartItems)}
-            />
+            {hideCheckbox ? (
+              <></>
+            ) : (
+              <BouncyCheckbox
+                iconStyle={{ borderColor: "lightgray", borderRadius: 0 }}
+                fillColor="green"
+                onPress={(checkboxValue) => selectItem(food, checkboxValue)}
+                isChecked={isFoodInCart(food, cartItems)}
+              />
+            )}
             <FoodInfo food={food} />
-            <FoodImage food={food} />
+            <FoodImage food={food} marginLeft={marginLeft ? marginLeft : 0} />
           </View>
           <Divider
             width={0.5}
@@ -108,11 +78,11 @@ const FoodInfo = (props) => (
   </View>
 );
 
-const FoodImage = (props) => (
+const FoodImage = ({ marginLeft, ...props }) => (
   <View>
     <Image
       source={{ uri: props.food.image }}
-      style={{ width: 90, height: 90, borderRadius: 8 }}
+      style={{ width: 90, height: 90, borderRadius: 8, marginLeft: marginLeft }}
     />
   </View>
 );
--- screens/OrderCompleted.js
+++ screens/OrderCompleted.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text } from "react-native";
 import LottieView from "lottie-react-native";
 
 import { useSelector } from "react-redux";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
+const querySnapshot = getDocs(collection(db, "orders"));
 
 export default function OrderCompleted() {
+  const [lastOrder, setLastOrder] = useState();
   const { items, restaurantName } = useSelector(
     (state) => state.cartReducer.selectedItems
   );
@@ -18,6 +23,14 @@ export default function OrderCompleted() {
     style: "currency",
     currency: "USD",
   });
+
+  useEffect(() => {
+    console.log("ORDUSER", querySnapshot.docs)
+    // querySnapshot.forEach((doc) => {
+    //   // doc.data() is never undefined for query doc snapshots
+    //   console.log(doc.id, " => ", doc.data());
+    // });
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
@@ -32,7 +45,7 @@ export default function OrderCompleted() {
           Your order at {restaurantName} has been placed for ${totalUSD} 🚀
         </Text>
         <LottieView
-          style={{ height: 200, alignSelf: "center"}}
+          style={{ height: 200, alignSelf: "center" }}
           source={require("../assets/animations/cooking2.json")}
           autoPlay
           speed={0.5}
